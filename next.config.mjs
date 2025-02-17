a1783828763ad5e7d366f683ba44a87822854774
Cenaure <127963429+Cenaure@users.noreply.github.com>
2025-02-17 16:22:30 +0200

    ffdsf

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,15 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'competition-green.vercel.app',
+        pathname: '/api/media/**',
+      },
+    ],
+  },
 }
 
 export default withPayload(nextConfig)
